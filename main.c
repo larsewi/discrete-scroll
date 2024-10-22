0594c39443b29772f21e9392ee14b0edeeea6e46
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-10-22 11:38:23 +0200

    Deamonize
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
 #include <ApplicationServices/ApplicationServices.h>
 
 #define SIGN(x) (((x) > 0) - ((x) < 0))
@@ -16,6 +26,50 @@ CGEventRef cgEventCallback(__attribute__((unused)) CGEventTapProxy proxy, __attr
 
 int main(void)
 {
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+    if (pid > 0)
+    {
+        return EXIT_SUCCESS;
+    }
+
+    if (setsid() < 0)
+    {
+        perror("setsid");
+        return EXIT_FAILURE;
+    }
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+    if (pid > 0)
+    {
+        return EXIT_SUCCESS;
+    }
+
+    umask(0);
+
+    chdir("/");
+    int x;
+    for (x = (int) sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close(x);
+    }
+
+    openlog("scrolld", LOG_PID, LOG_DAEMON);
+
+    syslog(LOG_NOTICE, PACKAGE_NAME " started");
+
     CFMachPortRef eventTap;
     CFRunLoopSourceRef runLoopSource;
 
@@ -28,6 +82,9 @@ int main(void)
 
     CFRelease(eventTap);
     CFRelease(runLoopSource);
+
+    syslog(LOG_NOTICE, "scrolld terminated");
+    closelog();
 
     return 0;
 }
